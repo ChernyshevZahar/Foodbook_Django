1fe8a8f9dadd410bcf717199671941cd2358fb75
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-10 23:32:45 +0300

    Создал модель CatigoryRecipe

--- FoodBook/RecipeAndProduct/models.py
+++ FoodBook/RecipeAndProduct/models.py
@@ -4,3 +4,7 @@ class Products(models.Model):
     name = models.CharField(max_length=30,blank=True)
     price = models.DecimalField(default=0,max_digits=8,decimal_places=2)
     callorise = models.IntegerField(null=False, blank=True)
+
+
+class CatigoryRecipe(models.Model):
+    name = models.CharField(max_length=50,blank=True)
