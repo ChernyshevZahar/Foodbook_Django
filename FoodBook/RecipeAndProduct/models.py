63f1730016bc646c1ab35a465f6824f85a721b7d
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-10 23:30:18 +0300

    Создал модель Products

--- FoodBook/RecipeAndProduct/models.py
+++ FoodBook/RecipeAndProduct/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=30,blank=True)
+    price = models.DecimalField(default=0,max_digits=8,decimal_places=2)
+    callorise = models.IntegerField(null=False, blank=True)
