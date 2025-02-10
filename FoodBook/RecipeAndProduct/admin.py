cb2bd41683f8859178b6d21bd5846ed3d83c5581
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-11 00:18:55 +0300

    Вывел модели:CatigoryRecipe, Recipe, Products в админку django

--- FoodBook/RecipeAndProduct/admin.py
+++ FoodBook/RecipeAndProduct/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import CatigoryRecipe, Recipe, Products
 
-# Register your models here.
+
+
+class CatigoryRecipeInLine(admin.TabularInline):
+    model = CatigoryRecipe.orders.through
+
+class ProductsInLine(admin.TabularInline):
+    model = Products.orders.through
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        CatigoryRecipeInLine,
+        ProductsInLine,
+    ]
+    list_display = ["name","discription","cooking_steps","cooking_time","image_dish"]
+
+@admin.register(CatigoryRecipe)
+class CatigoryRecipeAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ["name","price", "callorise"]
