0f34a44872aea0b8a49e07cc3806ce1d1c6a5d9a
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-11 17:21:05 +0300

    Добавил представления и маршруты для регистрации и logout

--- FoodBook/accounts/urls.py
+++ FoodBook/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from django.contrib.auth.views import LoginView
+from .views import MyLogoutView,RegisterUser,AboutMeView
 
 app_name='accounts'
 
@@ -9,4 +10,7 @@ urlpatterns = [
             template_name='accounts/login_myauth.html',
 
         ), name='login_app'),
+    path('logout/', MyLogoutView.as_view(), name='Logout_app'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('about-me/', AboutMeView.as_view(), name='about_me'),
 ]
--- FoodBook/accounts/views.py
+++ FoodBook/accounts/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .forms import ProfileEditForm
+from .models import Profile
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+
+from  django.views.generic import CreateView, TemplateView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from  django.urls import reverse_lazy
+
+class RegisterUser(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/register_user.html"
+    success_url = reverse_lazy('RecipeAndProduct:recipt-list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        Profile.objects.create(user=self.object)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password,
+
+        )
+        login(request=self.request, user=user)
+        return response
+
+class AboutMeView(TemplateView):
+    template_name = 'accounts/about-my.html'
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy('accounts:login_app')
