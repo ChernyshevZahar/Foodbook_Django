3687c1e839f1b4d2beba987457fb3a971b6c59c2
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-11 14:24:59 +0300

    Создал представления и маршрут для вывода рецептов

--- FoodBook/RecipeAndProduct/urls.py
+++ FoodBook/RecipeAndProduct/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import RecipeListView
+
+app_name='RecipeAndProduct'
 
 urlpatterns = [
-
+    path('recipts/', RecipeListView.as_view(), name = 'recipt-list')
 ]
--- FoodBook/RecipeAndProduct/views.py
+++ FoodBook/RecipeAndProduct/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Recipe
+
+class RecipeListView(ListView):
+    template_name = "RecipeAndProduct/recipe_list.html"
+    queryset = (
+        Recipe.objects
+        .prefetch_related('catigory_recipe',)
+        .all()
+    )
+    context_object_name = 'listrecipte'
