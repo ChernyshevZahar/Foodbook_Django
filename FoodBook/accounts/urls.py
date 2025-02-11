0032d6af3a083f488c0ecff3198a38d1daa48b6a
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-11 16:50:05 +0300

    Создал шаблон и маршруты для авторизации

--- FoodBook/accounts/urls.py
+++ FoodBook/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from django.contrib.auth.views import LoginView
 
 app_name='accounts'
 
 urlpatterns = [
+    path('login/', LoginView.as_view(
+            template_name='accounts/login_myauth.html',
 
+        ), name='login_app'),
 ]
