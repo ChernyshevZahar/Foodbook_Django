3ed720bdae8ba13d7cbe03b6d35ca30a47b5ba25
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-11 15:08:29 +0300

    Создал форму для добавления рецептов, создал шаблон , настроил маршруты и пердставление

--- FoodBook/RecipeAndProduct/urls.py
+++ FoodBook/RecipeAndProduct/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RecipeListView , RecipeDitailsView
+from .views import RecipeListView , RecipeDitailsView, CreateRecipteView
 
 app_name='RecipeAndProduct'
 
 urlpatterns = [
     path('recipts/', RecipeListView.as_view(), name = 'recipt-list'),
-    path('recipts/<int:pk>', RecipeDitailsView.as_view(), name = 'recipt-ditails')
+    path('recipts/<int:pk>', RecipeDitailsView.as_view(), name = 'recipt-ditails'),
+    path('recipts/create', CreateRecipteView.as_view(), name = 'recipt-add')
+
 ]
--- FoodBook/RecipeAndProduct/views.py
+++ FoodBook/RecipeAndProduct/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DetailView
+from django.views.generic import ListView , DetailView , CreateView
+
+from django.urls import reverse_lazy
 
 from .models import Recipe
-
+from .forms import AddingRecipeForm
 class RecipeListView(ListView):
     template_name = "RecipeAndProduct/recipe_list.html"
     queryset = (
@@ -21,3 +23,12 @@ class RecipeDitailsView(DetailView):
         .all()
     )
     context_object_name = 'recept'
+
+class CreateRecipteView(CreateView):
+    template_name = "RecipeAndProduct/recipe-adding.html"
+    form_class = AddingRecipeForm
+    success_url = reverse_lazy('RecipeAndProduct:recipt-list')
+    def form_valid(self, form):
+        recipte = form.save(commit=False)
+        recipte.save()
+        return super().form_valid(form)
