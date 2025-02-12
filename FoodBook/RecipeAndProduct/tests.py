61ab67bac971ed6e608f9a7ed4b8ec4e5c14a5f3
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-02-12 16:32:21 +0300

    Написал тест на представление  RecipeListView

--- FoodBook/RecipeAndProduct/tests.py
+++ FoodBook/RecipeAndProduct/tests.py
@@ -1,3 +1,33 @@
+from http.client import responses
+from django.contrib.auth.models import User
 from django.test import TestCase
+from  .models import Recipe
+from django.urls import reverse
 
-# Create your tests here.
+class TecipeDetailViewTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.User = User.objects.create_user(username="bob", password='12345')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.User.delete()
+
+    def setUp(self) -> None:
+        self.client.force_login(self.User)
+        self.recipe = Recipe.objects.create(
+            name='test',
+            discription = 'wdfwe',
+            cooking_steps = '1.2.3.4',
+            cooking_time = 30,
+            user = self.User
+        )
+
+    def tearDown(self):
+        self.recipe.delete()
+
+    def test_recipe_details(self):
+        respons = self.client.get(reverse('RecipeAndProduct:recipt-list'))
+        self.assertContains(respons, self.recipe.name)
+        self.assertContains(respons, self.recipe.cooking_time)
+        self.assertContains(respons, self.recipe.pk)
